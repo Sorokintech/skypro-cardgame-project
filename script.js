9098473b4b10c312c28a90bc0b3b6c751bd44568
Sorokintech <sorokintech@gmail.com>
2022-07-27 11:43:51 +0300

    template-fix

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('Content loaded');
-    window.application.renderBlock('startPage', app);
-    window.application.renderScreen('startPage', app);
-   });
-const app = document.querySelector('.app');
--- src/startPage.js
+++ src/startPage.js
@@ -1,38 +1,40 @@
-function renderStartPageHTML(container) {
-    const mainContainer = document.createElement('div');
-        mainContainer.classList.add('container');
-    const containerHeader = document.createElement('h1');
-        containerHeader.classList.add('container-header');
-    const chooseDifficultyContainer = document.createElement('div');
-        chooseDifficultyContainer.classList.add('difficulty-container');
-    const firstDifficultyButton = document.createElement('button');
-        firstDifficultyButton.classList.add('difficulty-button');
-    const secondDifficultyButton = document.createElement('button');
-        secondDifficultyButton.classList.add('difficulty-button');
-    const thirdDifficultyButton = document.createElement('button');
-        thirdDifficultyButton.classList.add('difficulty-button');
-    const startButton = document.createElement('button');
-        startButton.classList.add('start-button');
+const app = document.querySelector('.app');
 
-    container.appendChild(mainContainer);
-    mainContainer.appendChild(containerHeader);
-    mainContainer.appendChild(chooseDifficultyContainer);
-    chooseDifficultyContainer.appendChild(firstDifficultyButton);
-    chooseDifficultyContainer.appendChild(secondDifficultyButton);
-    chooseDifficultyContainer.appendChild(thirdDifficultyButton);
-    mainContainer.appendChild(startButton);
-  };
-  function renderStartPageContent(container) {
-    const containerHeader = container.querySelector('.container-header');
-    containerHeader.textContent = 'Выбери сложность';
-    const difficultyButtons = container.querySelectorAll('.difficulty-button');
-    console.log(difficultyButtons);
-        difficultyButtons[0].textContent = '1';
-        difficultyButtons[1].textContent = '2';
-        difficultyButtons[2].textContent = '3';
-    const startButton = container.querySelector('.start-button');
-    console.log(startButton);
-    startButton.textContent = 'Старт';  
-}
-window.application.blocks['startPage'] = renderStartPageHTML; 
-window.application.screens['startPage'] = renderStartPageContent;
+
+const templateStartPage ={
+    tag : 'div',
+    cls: 'container',
+    content: [
+        {
+            tag: 'div',
+            cls: 'container-header',
+            content: 'Выбери сложность',
+        }, 
+        {
+            tag: 'div',
+            cls: 'difficulty-container',
+            content: [
+                {
+                    tag: 'button',
+                    cls: 'difficulty-button',
+                    content: '1'
+                },
+                {
+                    tag: 'button',
+                    cls: 'difficulty-button',
+                    content: '2'
+                },
+                {
+                    tag: 'button',
+                    cls: 'difficulty-button',
+                    content: '3'
+                },
+            ]
+        },
+        {
+            tag: 'button',
+            cls: 'start-button',
+            content: 'Старт'
+        }
+    ]
+};
